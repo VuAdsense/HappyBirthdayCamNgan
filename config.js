4d960a819f11a8f8c46ee4fc9379dc7a9767def5
VuAdsense <148184720+VuAdsense@users.noreply.github.com>
2023-10-17 12:44:07 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '18 - 10 - 2003';
+const giftUrl = 'https://www.facebook.com/ngan.cam.777701';
+const eventName = 'Happy Birthday Ngân';
+const titleCard = 'Happy Birthday Cẩm Ngân';
+const contentCard = 'Chúc Cẩm Ngân tuổi mới vui vẻ, tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Ngân trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
